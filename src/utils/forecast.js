fbed2097ea3d4d420a5e057cc8edeaacd5fb7370
snubtac <65017989+snubtac@users.noreply.github.com>
2020-09-26 16:12:21 +0800

    Add additional info to forecast.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
     } else if (body.message) {
       callback('Unable to find location.', undefined)
     } else {
-      callback(undefined, body.daily[0].weather[0].description.toUpperCase() + '. It is currently ' + body.current.temp + ' degrees out. There is a ' + Math.round(body.hourly[0].pop*100) + '% chance of rain.')
+      callback(undefined, 'Today\'s weather forecast: ' + body.daily[0].weather[0].description.toUpperCase() + '. Min and max temperatures are ' + body.daily[0].temp.min + '\xB0c and ' + body.daily[0].temp.max + '\xB0c, respectively. It is currently ' + body.current.temp + '\xB0c out. There is a ' + Math.round(body.daily[0].pop*100) + '% chance of rain today.')
     }
   })
 };
